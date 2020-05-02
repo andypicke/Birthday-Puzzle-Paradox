7077be6b2ee3814eaa80694148a2793b5e334d4a
Andy <andypicke@gmail.com>
2020-05-02 16:59:24 -0600

    Update README, script, images

--- README.md
+++ README.md
@@ -13,22 +13,43 @@ I'll first use a simulation-based approach in python to solve the traditional pr
 - Repeat this sampling process many times, then calculate the percent of times we get at least one duplicate; this is interpreted as the probability of getting at least one match for that room size.
 - Repeat the above process for different *N_in_room* values, and plot the probabilities versus *N_in_room*.
 
-
+The results look similar to previous solutions, with the probability reaching 50% at a room size of 23 people:
 ![](images/p_gte2_vs_N_uniform.png)
+
+We can also easily ask other variants of the question, like "What is the probability that 3 or more people in the room share a birthday?" The probability of at least 3 people sharing a birthday reaches ~50% at a room size of 88 people:
 
 ![](images/p_gte2_gte3_vs_N_uniform.png)
 
 
 ## Solution Using Actual Probabilities of Birth Days
 
-In the above solution, we assumed that all birthdays were equally likely; but this is not the case in real life. A friend recently shared an interesting [visualization of birthdays](http://www.vizwiz.com/2012/05/how-common-is-your-birthday-find-out.html), which reminded me of the Birthday Puzzle and made me wonder how the results would change if we used the actual birthday probabilities (instead of assuming a uniform distribution). 
+In the above solution, we assumed that all birthdays were equally likely; but this is not the case in real life. A friend recently shared an interesting [visualization of most common birthdays](http://www.vizwiz.com/2012/05/how-common-is-your-birthday-find-out.html), which reminded me of the Birthday Puzzle and made me wonder how the results would change if we used the actual birthday probabilities (instead of assuming a uniform distribution). 
 
-The approach to solving the problem is the same as before, _except_ we assign a probability based on birth data to each day, instead of assuming a uniform distribution.
+The approach to solving the problem is the same as before, _except_ we assign a probability based on birth data to each day, instead of assuming a uniform distribution. I used [data on US births from 2000-2014 compiled by FiveThirtyEight](https://github.com/fivethirtyeight/data/tree/master/births), which I learned of from this [blog post](http://thedailyviz.com/2016/09/17/how-common-is-your-birthday-dailyviz/). 
 
-- http://thedailyviz.com/2016/09/17/how-common-is-your-birthday-dailyviz/
-- https://github.com/fivethirtyeight/data/tree/master/births
+First I summed births by month and day and visualized the results with a heatmap, producing a plot similar to those in the two blog posts mentioned above. The main takeaways are:
+- There is a seasonal pattern: there tend to be more births in September +/- a month. You can do the math and see what that infers about when babies are _conceived_.
+- There tend to be many less births on or close to major holidays (Thanksgiving, Christmas, New Year's, July 4th). I'm not sure if this is more due to people not wanting to give birth on holidays, or doctors not wanting to work/not available on holidays. But I thought it was interesting that it implies that in a large percentage of pregnancies, there is some leeway/choice (at least a day or two) of when exactly to give birth.
+
+![](images/births_heatmap.png)
 
 
-![](images/births_heatmap.png)*Caption*
+Next, to calculate the probability of being born for each day I:
 
-![](images/birth_prob_vs_yday.png)*Caption*
+- Kept only data from non-leap years
+- Turned dates into yeardays (1-365) and summed births over each yearday
+- Divided those values by the total number of births over all days to calculate probabilities, which sum to 1. 
+
+These probabilities are shown in the following figure, as well as the uniform probability (1/365) indicated by the horizontal line. Although the true probabilities are definitely not uniform, the difference in magnitude isn't that large.
+
+![](images/birth_prob_vs_yday.png)
+
+Intuitively, I expected that using the actual probabilities would increase the probability. However, running the simulation again using the actual probabilities, I found that the results really weren't affected significantly. 
+
+![](images/p_gte2_vs_N_uniform_actual.png)
+
+To test whether my intuition (and/or code) was correct, I created a fake probability distribution with a similar but more drastic seasonal peak and ran the simulations again. This time the curve is indeed shifted to the left, meaning the probability of 2 people sharing a birthday for a given room size is increased (though still not by that much).
+
+![](images/fake_birth_prob_vs_yday.png)
+
+![](images/p_gte2_vs_N_uniform_fakeprobs.png)
--- src/birthday_puzzle.py
+++ src/birthday_puzzle.py
@@ -2,7 +2,6 @@
 
 import pandas as pd
 import numpy as np
-np.set_printoptions(suppress=True)
 import matplotlib.pyplot as plt
 from collections import Counter
 import seaborn as sns
@@ -23,7 +22,7 @@ def prob_shared_birthday(N_in_room, N_simulations=5000, probs=None):
     N_in_room (integer) : Number of People in room
     N_simulations (integer)(optional) : Number of Simulations to run (default = 5000)
     probs (numpy array)(optional): Array of probabilities for each yday (1-365). 
-        Defaults to uniform probability distribution
+        Defaults to 'None' (uniform probability distribution)
     
     RETURNS
     (p_gte2, p_gte3) (tuple of floats) : Probabilites of >=2, >=3 people sharing birthdays
@@ -40,33 +39,35 @@ def prob_shared_birthday(N_in_room, N_simulations=5000, probs=None):
     p_gte3 = np.mean(np.array(n_gte3)>0)
     return (p_gte2, p_gte3)
 
+plt_size = (6,6)
 
 if __name__=='__main__':
+
+
+    N_sims = 5000
 
     p2=[]
     p3=[]
     for N_in_room in range(1,100):
-        p_gte2, p_gte3 = prob_shared_birthday(N_in_room)
+        p_gte2, p_gte3 = prob_shared_birthday(N_in_room, N_simulations=N_sims)
         p2.append(p_gte2)
         p3.append(p_gte3)
     
-    fig, ax = plt.subplots(1, figsize=(8,8))
+    fig, ax = plt.subplots(1, figsize=plt_size)
     ax.plot(list(range(1,100)), p2, linewidth=3)
     ax.set_xlabel('# People in Room')
     ax.set_ylabel('Probability')
-    ax.set_title('Probability At Least 2 People Share Birthday')
+    ax.set_title('Prob. >=2 People Share Birthday (' + str(N_sims) + ' Simulations)')
     plt.savefig('./images/p_gte2_vs_N_uniform.png')
 
-    fig, ax = plt.subplots(1, figsize=(8,8))
+    fig, ax = plt.subplots(1, figsize=plt_size)
     ax.plot(list(range(1,100)), p2, linewidth=3, label='>=2')
     ax.plot(list(range(1,100)), p3, linewidth=3, label='>=3')
     ax.set_xlabel('# People in Room')
     ax.set_ylabel('Probability')
     ax.legend()
-    ax.set_title('Probability At Least N People Share Birthday')
+    ax.set_title('Prob >=N People Share Birthday (' + str(N_sims) + ' Simulations)')
     plt.savefig('./images/p_gte2_gte3_vs_N_uniform.png')
-
-
     
 
     df_births = pd.read_csv('./data/US_births_2000-2014_SSA.csv')
@@ -75,10 +76,10 @@ if __name__=='__main__':
     df_births['date'] = pd.to_datetime(df_births[['year', 'month', 'day']])
     df_births['leap_year'] = df_births['date'].dt.is_leap_year
     df_births['yearday'] = df_births['date'].dt.dayofyear
-    # drop leap years
     df_births = df_births[df_births['leap_year']==False]
     df_births.drop('leap_year', axis=1, inplace=True)
     
+    # Make heatmap of total births by month and day
     df2 = df_births.groupby(['month','day']).sum().reset_index()
     fig,ax = plt.subplots(1, figsize=(14,8))
     sns.heatmap(df2.pivot('month','day','births'), ax=ax)
@@ -88,13 +89,15 @@ if __name__=='__main__':
     ax.set_title('Number of Births Vs. Month and Day')
     plt.savefig('images/births_heatmap.png')
 
+    # calculate probability of being born each day from data
     total_births = df_births['births'].sum()
     df_births.drop(['year', 'month', 'day'], axis=1, inplace=True)
     df_gb_yday = df_births.groupby('yearday').sum().reset_index()
     df_gb_yday['birth_prob'] = df_gb_yday['births']/total_births
     df_gb_yday
 
-    fig, ax = plt.subplots(1, figsize=(8,8))
+    # Plot probabilities vs yearday
+    fig, ax = plt.subplots(1, figsize=plt_size)
     ax.scatter(df_gb_yday['yearday'], df_gb_yday['birth_prob'], label='Actual Probs')
     ax.set_ylim(0,0.0035)
     ax.axhline(1/365, color='black', label='Uniform P (1/365)')
@@ -102,3 +105,48 @@ if __name__=='__main__':
     ax.set_ylabel('Probability')
     ax.legend()
     plt.savefig('images/birth_prob_vs_yday.png')
+
+    # Run simulations w/ uniform distribution and actual probs
+    p1=[]
+    for N_in_room in range(1,100):
+        p_gte2, p_gte3 = prob_shared_birthday(N_in_room, N_simulations=N_sims, probs=None)
+        p1.append(p_gte2)
+
+    p2=[]
+    for N_in_room in range(1,100):
+        p_gte2, p_gte3 = prob_shared_birthday(N_in_room, N_simulations=N_sims, probs = df_gb_yday['birth_prob'].values)
+        p2.append(p_gte2)
+
+    fig, ax = plt.subplots(1, figsize=plt_size)
+    ax.plot(list(range(1,100)), p1, linewidth=3, label='Uniform Dist.')
+    ax.plot(list(range(1,100)), p2, linewidth=3, label='W/ Probs')
+    ax.set_xlabel('# People in Room')
+    ax.set_ylabel('Probability')
+    ax.legend()
+    ax.set_title('Prob >=2 People Share Birthday (' + str(N_sims) + ' Simulations)')
+    plt.savefig('images/p_gte2_vs_N_uniform_actual.png')
+
+    ydays = np.arange(1,366)
+    test_probs = np.sin(ydays/120)
+    test_probs = test_probs/np.sum(test_probs)
+
+    fig, ax = plt.subplots(1, figsize=plt_size)
+    ax.plot(ydays,test_probs)
+    ax.set_xlabel('Yearday')
+    ax.set_ylabel('Probability')
+    ax.set_title('Fake Probability Distribution')
+    plt.savefig('images/fake_birth_prob_vs_yday.png')
+
+    p3=[]
+    for N_in_room in range(1,100):
+        p_gte2, p_gte3 = prob_shared_birthday(N_in_room, N_simulations=N_sims, probs = test_probs)
+        p3.append(p_gte2)
+
+    fig, ax = plt.subplots(1, figsize=plt_size)
+    ax.plot(list(range(1,100)), p1, linewidth=3, label='Uniform Dist.')
+    ax.plot(list(range(1,100)), p3, linewidth=3, label='W/ Fake Probs')
+    ax.set_xlabel('# People in Room')
+    ax.set_ylabel('Probability')
+    ax.legend()
+    ax.set_title('Prob >=2 People Share Birthday (' + str(N_sims) + ' Simulations)')
+    plt.savefig('images/p_gte2_vs_N_uniform_fakeprobs.png')
